f5b4be3b7b5baa13f644192a393818f359ebfee4
NandreyN <nandreyn1998@gmail.com>
2017-02-07 20:40:18 +0300

    fixes

--- Sem2Lab1/Tasks.cpp
+++ Sem2Lab1/Tasks.cpp
@@ -11,6 +11,8 @@ using namespace std;
 
 void Tasks::TaskOne()
 {
+	cout << "======================================================================================================================" << endl;
+	cout << "TASK 1" << endl;
 	// Create vector<char> which contains symbols of alphabet
 	//Print in direct and reverse order
 
@@ -24,6 +26,7 @@ void Tasks::TaskOne()
 	});
 
 	//Reverse
+	cout << "__________________________________________________________________________________________________________________" << endl;
 	cout << "Reverse order : " << endl << endl;
 	for_each(charVector.rbegin(), charVector.rend(), [](char c)->void {
 		cout << c << endl;
@@ -32,6 +35,8 @@ void Tasks::TaskOne()
 
 void Tasks::TaskTwo(const string& sourcePath, char filterParam) throw (invalid_argument)
 {
+	cout << "======================================================================================================================" << endl;
+	cout << "TASK 2" << endl;
 	ifstream inputFile;
 	inputFile.open(sourcePath);
 	if (!inputFile.is_open())
@@ -51,15 +56,17 @@ void Tasks::TaskTwo(const string& sourcePath, char filterParam) throw (invalid_a
 
 
 	lst.sort([](const string& a, const string& b)->bool {return a < b; });
-
+	cout << "__________________________________________________________________________________________________________________" << endl;
+	cout << "Sorted" << endl;
 	for_each(lst.begin(), lst.end(), [](auto s) {cout << s << endl; }); // printing
 
-	cout << "Printed  by letter : " << endl << "/////////////////////////////////////////////////" << endl;
+	cout << "__________________________________________________________________________________________________________________" << endl;
+	cout << "Printed  by letter : " << endl;
 	// Printing only strings started with filterParam letter
 
 	for_each(lst.begin(), lst.end(), [filterParam](const string& a)->void
 	{
-		if (a.c_str()[0] == filterParam)
+		if (a[0] == filterParam)
 			cout << a << endl;
 	});
 
@@ -75,17 +82,18 @@ void Tasks::TaskTwo(const string& sourcePath, char filterParam) throw (invalid_a
 	/*Implement delete analog here*/
 	remove_if(lst.begin(), lst.end(), [filterParam](string a)->bool
 	{
-		return a.c_str()[0] == filterParam;
+		return a[0] == filterParam;
 	});
 
-
+	cout << "__________________________________________________________________________________________________________________" << endl;
 	cout << "After Deleting : " << endl;
 	for_each(lst.begin(), lst.end(), [](auto s) {cout << s << endl; }); // printing
-	cout << "done!" << endl;
 }
 
 void Tasks::TaskThree(const std::string& sourcePath, int compare1, int compare2, bool greater)
 {
+	cout << "========================================================================================================================" << endl;
+	cout << "TASK 3" << endl;
 	vector<int> vector;
 	list<int> list;
 	deque<int> deque;
@@ -111,7 +119,7 @@ void Tasks::TaskThree(const std::string& sourcePath, int compare1, int compare2,
 	int value2List = count(list.begin(), list.begin(), compare1);
 	int value3Deque = count(deque.begin(), deque.begin(), compare1);
 
-	cout << "task 1  : equel to " << to_string(compare1) << endl;
+	cout << "task 3.1  : equel to " << to_string(compare1) << endl;
 	cout << "Vector : " << value1Vector << endl;
 	cout << "List : " << value2List << endl;
 	cout << "Deque : " << value3Deque << endl;
@@ -130,12 +138,12 @@ void Tasks::TaskThree(const std::string& sourcePath, int compare1, int compare2,
 
 	string messagetoOut = (greater) ? "greater than " : "less than";
 	cout << "__________________________________________________________________________________________________________________" << endl;
-	cout << "task 2  : " << messagetoOut << compare2 << endl;
+	cout << "task 3.2  : " << messagetoOut << compare2 << endl;
 	cout << "Vector : " << value1Vector << endl;
 	cout << "List : " << value2List << endl;
 	cout << "Deque : " << value3Deque << endl;
 	cout << "__________________________________________________________________________________________________________________" << endl;
-	cout << "task 3 : sizes" << endl;
+	cout << "task 3.3 : sizes" << endl;
 	cout << "Vector : " << vector.size() << endl;
 	cout << "List : " << list.size() << endl;
 	cout << "Deque : " << deque.size() << endl;
@@ -144,12 +152,12 @@ void Tasks::TaskThree(const std::string& sourcePath, int compare1, int compare2,
 	value2List = accumulate(list.begin(), list.end(), 0);
 	value3Deque = accumulate(deque.begin(), deque.end(), 0);
 	cout << "__________________________________________________________________________________________________________________" << endl;
-	cout << "task 4: sum" << endl;
+	cout << "task 3.4: sum" << endl;
 	cout << "Vector : " << value1Vector << endl;
 	cout << "List : " << value2List << endl;
 	cout << "Deque : " << value3Deque << endl;
 	cout << "__________________________________________________________________________________________________________________" << endl;
-	cout << "task 5: average" << endl;
+	cout << "task 3.5: average" << endl;
 	cout << "Vector : " << static_cast<double>(value1Vector)/vector.size()<< endl;
 	cout << "List : " << static_cast<double>(value2List)/ list.size()<< endl;
 	cout << "Deque : " << static_cast<double>(value3Deque)/ deque.size() << endl;
--- Sem2Lab1/main.cpp
+++ Sem2Lab1/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-	//Tasks::TaskOne();
-	//Tasks::TaskTwo("inT2.txt", 'H');
+	Tasks::TaskOne();
+	Tasks::TaskTwo("inT2.txt", 'H');
 	Tasks::TaskThree("inT3.txt", 5,15,true);
 }
